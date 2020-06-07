e713087e2d1a955be3602729b05e9004b766bd4e
Ubuntu <ubuntu@ip-172-31-24-61.us-east-2.compute.internal>
2020-06-07 02:17:49 +0000

    Cleaned Code

---
+++ src/functions.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve, auc
+
+def preprocess_image(image, imgage_shape=[100,100]):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, imgage_shape)
+    image /= 255.0  # normalize to [0,1] range
+    return image
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
+
+def create_X_y(df):
+    y = df['diagnosis']
+    X = []
+    for i in df['image']:
+        image = load_and_preprocess_image(i)
+        X.append(np.asarray(image))
+    return np.asarray(X), np.asarray(y)
+
+
+def plot_accuracy(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+def plot_recall(history, recall):
+    plt.plot(history.history[recall])
+    plt.plot(history.history['val_'+recall])
+    plt.title('model recall')
+    plt.ylabel('recall')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+    
+
+def matrix_score(y_test, y_pred):
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    print(np.array([[tp, fn],[fp, tn]]))
+    print('Recall:    {:.3f}'.format(tp/(tp+fn)))
+    print('Precision: {:.3f}'.format(tp/(tp+fp)))
+    print('Accuracy:  {:.3f}'.format((tp+tn)/(tp+tn+fp+fn)))
+    print('ROC AUC:   {:.3f}'.format(roc_auc_score(y_test, y_pred)))
